ff5686b97f5617f06e663e6a2f37dbddc12a53af
jie.jia <jiajie8362@gmail.com>
2018-01-29 15:01:39 +0800

    [JJ] step 3

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react'
 import CommentInput from './CommentInput'
 import CommentList from './CommentList'
+import wrapwithLoadData from './wrapWithLoadData';
 
 class CommentApp extends Component {
-  constructor() {
-    super();
+  static propTypes = {
+    data: PropTypes.any,
+    saveData: PropTypes.func.isRequired
+  }
+
+  constructor(props) {
+    super(props);
     this.state = {
-      comments: []
+      comments: props.data
     }
   }
+
   handleSubmitComment (comment) {
     if (!comment) return
     if (!comment.username) return alert('请输入用户名')
     if (!comment.content) return alert('请输入评论内容')
-    this.state.comments.push(comment)
-    this.setState({
-      comments: this.state.comments
-    })
+    const comments = this.state.comments
+    comments.push(comment)
+    this.setState({ comments })
+    this.props.saveData(comments)
+  }
+
+  handleDeleteComment(index) {
+    const comments = this.state.comments;
+    comments.splice(index, 1);
+    this.setState({comments});
+    this.props.saveData(comments);
   }
 
   render() {
-    return(
-      <div className="wrapper">
-        <CommentInput onSubmit={this.handleSubmitComment.bind(this)}/>
-        <CommentList comments={this.state.comments}/>
+    return (
+      <div className='wrapper'>
+        <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
+        <CommentList
+          comments={this.state.comments}
+          onDeleteComment={this.handleDeleteComment.bind(this)} />
       </div>
     )
   }
 }
 
+CommentApp = wrapwithLoadData(CommentApp, 'comments')
 export default CommentApp
