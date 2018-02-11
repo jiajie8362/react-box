41520ae6ffc5d1d7136c2766233beaf187fa84e4
jie.jia <jiajie8362@gmail.com>
2018-02-11 19:13:02 +0800

    [JJ] add base info

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -3,11 +3,12 @@ import Logo from '../../component/logo/logo'
 import {List, InputItem, WingBlank, WhiteSpace, Button} from 'antd-mobile'
 import { connect } from 'react-redux'
 import {Redirect} from 'react-router-dom'
-import {register} from '../../redux/user.redux'
+import {login} from '../../redux/user.redux'
+
 
 @connect(
   state=>state.user,
-  {register}
+  {login}
 )
 class Login extends React.Component{
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { BrowserRouter, Route, Redirect,Switch } from 'react-router-dom'
 import Login from './container/login/login'
 import Register from './container/register/register'
 import AuthRoute from './component/authroute/authroute'
+import BossInfo from './container/bossinfo/bossinfo'
+import GeniusInfo from './container/geniusinfo/geniusinfo'
 import reducers from './reducer'
 import './config'
 import './index.css'
@@ -16,15 +18,14 @@ const store = createStore(reducers, compose(
   applyMiddleware(thunk),
   window.devToolsExtension?window.devToolsExtension():f=>f
 ))
-function Boss(){
-  return <h2>BOSS页面</h2>
-}
+
 ReactDom.render(
   (<Provider store={store}>
     <BrowserRouter>
       <div>
         <AuthRoute></AuthRoute>
-        <Route path='/boss' component={Boss}></Route>
+        <Route path='/bossinfo' component={BossInfo}></Route>
+        <Route path='/geniusinfo' component={GeniusInfo}></Route>
         <Route path='/login' component={Login}></Route>
         <Route path='/register' component={Register}></Route>
       </div>
